8b68b7da2a0a59756f0223d72b4a823e00360e06
zhaoyang <18355403288@163.com>
2018-12-18 17:34:36 +0800

    udpate

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+// import css
 import '@/bower_components/bootstrap/dist/css/bootstrap.min.css';
 import '@/bower_components/font-awesome/css/font-awesome.min.css';
 import '@/bower_components/Ionicons/css/ionicons.min.css';
 import '@/dist/css/AdminLTE.min.css';
 
+// import js
 import '@/bower_components/jquery/dist/jquery.min.js';
 import '@/bower_components/bootstrap/dist/js/bootstrap.min.js';
 // test
